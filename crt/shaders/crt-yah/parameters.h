ec1c525acd8d29555bfcccb9b91dd105a24c74f5
Jezze <jezze@gmx.net>
2026-02-17 20:23:23 +0100

    Add NTSC auto phase option

--- crt/shaders/crt-yah/parameters.h
+++ crt/shaders/crt-yah/parameters.h
@@ -50,7 +50,7 @@
 
 // NTSC parameters
 #pragma parameter NTSC_PROFILE "·  NTSC > Profile  (0-Off, 1-Separate Y/C, 2-Composite, 3-RF)" 0.0 0.0 3.0 0.1
-#pragma parameter NTSC_QUALITY "   NTSC > Chroma Phase  (2-Two Phase, 3-Three Phase)" 2.0 2.0 3.0 1.0
+#pragma parameter NTSC_QUALITY "   NTSC > Chroma Phase  (1-Auto, 2-Two, 3-Three)" 2.0 1.0 3.0 1.0
 #pragma parameter NTSC_SHIFT "   NTSC > Chroma Shift  (-left .. +right)" 0.0 -1.0 1.0 0.1
 #pragma parameter NTSC_SCALE "   NTSC > Scale⁵  (-Down / 0-Auto / +Up)" 0.0 -0.5 0.5 0.05
 #pragma parameter NTSC_JITTER "   NTSC > Jitter⁴  (0-None .. 1-Full)" 1.0 0.0 2.0 0.1
